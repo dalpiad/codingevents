c01d6c42d272d3428792b6b12a6d2e07f1ac9490
Doyle Brown <dalpiad@gmail.com>
2024-02-06 21:16:29 -0600

    Setup the tags class without relationship

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,14 +1,14 @@
 package org.launchcode.codingevents.models;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Chris Bay
@@ -23,6 +23,9 @@ public class Event extends AbstractEntity {
     @ManyToOne()
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
+
+    @ManyToMany
+    private final List<Tag> tags = new ArrayList<>();
 
     @OneToOne(cascade = CascadeType.ALL)
     @Valid
@@ -65,4 +68,10 @@ public class Event extends AbstractEntity {
         return name;
     }
 
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
+    }
+    public List<Tag> getTags() {
+        return tags;
+    }
 }
